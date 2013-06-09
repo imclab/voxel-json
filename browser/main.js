52f86598bd8c5b013df4188cc13f8bab0a849895
James Halliday <mail@substack.net>
2013-06-09 13:35:38 -0700

    generate and apply diffs

--- browser/main.js
+++ browser/main.js
@@ -6,7 +6,26 @@ var game = require('voxel-hello-world')({
     materials: [ 'grass', 'brick', 'dirt' ]
 });
 
+var diff = {};
+window.diff = diff;
+
+window.applyDiff = applyDiff;
+
+function applyDiff (diff) {
+    Object.keys(diff).forEach(function (key) {
+        var pos = keyToPos(key);
+        game.setBlock(pos, diff[key]);
+    });
+}
+
+game.on('setBlock', function (pos, value) {
+    diff[posToKey(pos)] = value;
+});
+
 function generate (x, y_, z) {
     var y = y_ + 25;
     return x*x + y*y + z*z < 400;
 }
+
+function posToKey (pos) { return pos.join('|') }
+function keyToPos (key) { return key.split('|') }
